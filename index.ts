008397772ecdc090b56eee6ccd8a2bb132ad46ad
denis.s <denis.s@admins-Mac-mini-2.local>
2020-11-10 17:17:17 +0200

    change export

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import Wallet from "./wallet/Wallet";
 import bitcoreKaspaSetup from './wallet/bitcoreKaspaSetup';
 
-module.exports.Wallet = Wallet;
-module.exports.bitcoreKaspaSetup = bitcoreKaspaSetup;
+// module.exports.Wallet = Wallet;
+// module.exports.bitcoreKaspaSetup = bitcoreKaspaSetup;
+
+export default {Wallet, bitcoreKaspaSetup}
