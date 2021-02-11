a0f47edbb280bd9dc5c273516af32c75a1480a14
surinder singh <surinder83singh@gmail.com>
2021-02-11 15:38:38 +0530

    flow-key-crypt, browser-passworder deps removed

--- types/flow-key-crypt/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@aspectron/flow-key-crypt' {
-
-  export function encrypt(password: string, data: string): Promise<string>;
-  export function decrypt(password: string, encrypted: string): Promise<string>;
-}
